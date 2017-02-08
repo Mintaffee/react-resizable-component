081d0542db9042229c26fe1b8be6d89e5938b5dd
Jan <jan.vaclavik2@gmail.com>
2017-02-08 19:41:45 +0100

    App cleanup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Stylesheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div> Blank App </div>
+    );
+  }
+}
+
+export default App;
